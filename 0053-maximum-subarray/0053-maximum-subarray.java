c485a1c4925f0f566d1158caa5c1325aa95c7ad8
Toujar <kundenayaktoujar@gmail.com>
2026-07-08 10:30:00 +0530

    Time: 1 ms (99.96%), Space: 76.9 MB (95.91%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = Integer.MIN_VALUE;
+        int temp = 0;
+        
+        for(int i=0;i<nums.length;i++){
+            temp = temp + nums[i];
+            sum = (sum>temp)?sum:temp;
+            if(temp < 0){
+                temp = 0;
+            }
+        }
+        return sum;
+    }
+}
