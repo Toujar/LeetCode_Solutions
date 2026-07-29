4822b5fdeaac59bf54ecaba296e211b365f91f09
Toujar <kundenayaktoujar@gmail.com>
2026-07-29 18:31:21 +0530

    Time: 4 ms (82.62%), Space: 44.3 MB (82.8%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] ar = new int[26];
+        if(s.length() != t.length()){
+            return false;
+        }
+        for(int i=0;i<s.length();i++){
+            ar[s.charAt(i)-'a'] += 1;
+        }
+        for(int i=0;i<t.length();i++){
+            if(ar[t.charAt(i)-'a']==0){
+                return false;
+            }
+            ar[t.charAt(i)-'a'] -= 1;
+        }
+        return true;
+    }
+}
